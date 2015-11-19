48b38d3044dbe0e6f234d2ff91870661ec7de199
Alireza <alireza_paridar@outlook.com>
2015-11-19 15:08:09 +0330

    edit version

--- MessageParser.NET/Properties/AssemblyInfo.cs
+++ MessageParser.NET/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MessageParser.Net")]
 [assembly: AssemblyDescription("MessageParser.Net Is Simple Message Parser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("AlirezaP")]
+[assembly: AssemblyCompany("Alireza Paridar")]
 [assembly: AssemblyProduct("MessageParser.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
